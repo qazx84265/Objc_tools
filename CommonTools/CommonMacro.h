eaeebe12d315e0694fac4ac8545de4b6a420a111
qazx84265 <qazx84265@163.com>
2015-11-03 14:55:29 +0800

    Update CommonMacro.h

--- CommonTools/CommonMacro.h
+++ CommonTools/CommonMacro.h
@@ -10,36 +10,43 @@
 #define CommonMacro_h
 
 
-//-------------------获取设备大小-------------------------
+#pragma mark -------------------获取设备大小-------------------------
 //NavBar高度
-#define NavigationBar_HEIGHT 44
+#define kNAVIGATION_BAR_HEIGHT 44
 //获取屏幕 宽度、高度
-#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
-#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+#define kSCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define kSCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
 //-------------------获取设备大小-------------------------
-//-------------------打印日志-------------------------
+
+
+
+#pragma mark -------------------打印日志-------------------------
 //DEBUG 模式下打印日志,当前行
 #ifdef DEBUG
 # define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
 # define DLog(...)
 #endif
+
 //重写NSLog,Debug模式下打印日志和当前行数
 #if DEBUG
 #define NSLog(FORMAT, ...) fprintf(stderr,"\nfunction:%s line:%d content:%s\n", __FUNCTION__, __LINE__, [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
 #else
 #define NSLog(FORMAT, ...) nil
 #endif
+
 //DEBUG 模式下打印日志,当前行 并弹出一个警告
 #ifdef DEBUG
 # define ULog(fmt, ...) { UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
 #else
 # define ULog(...)
 #endif
+
 #define ITTDEBUG
 #define ITTLOGLEVEL_INFO 10
 #define ITTLOGLEVEL_WARNING 3
 #define ITTLOGLEVEL_ERROR 1
+
 #ifndef ITTMAXLOGLEVEL
 #ifdef DEBUG
 #define ITTMAXLOGLEVEL ITTLOGLEVEL_INFO
@@ -47,30 +54,36 @@
 #define ITTMAXLOGLEVEL ITTLOGLEVEL_ERROR
 #endif
 #endif
+
 // The general purpose logger. This ignores logging levels.
 #ifdef ITTDEBUG
 #define ITTDPRINT(xx, ...) NSLog(@"%s(%d): " xx, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
 #define ITTDPRINT(xx, ...) ((void)0)
 #endif
+
 // Prints the current method's name.
 #define ITTDPRINTMETHODNAME() ITTDPRINT(@"%s", __PRETTY_FUNCTION__)
+
 // Log-level based logging macros.
 #if ITTLOGLEVEL_ERROR <= ITTMAXLOGLEVEL
 #define ITTDERROR(xx, ...) ITTDPRINT(xx, ##__VA_ARGS__)
 #else
 #define ITTDERROR(xx, ...) ((void)0)
 #endif
+
 #if ITTLOGLEVEL_WARNING <= ITTMAXLOGLEVEL
 #define ITTDWARNING(xx, ...) ITTDPRINT(xx, ##__VA_ARGS__)
 #else
 #define ITTDWARNING(xx, ...) ((void)0)
 #endif
+
 #if ITTLOGLEVEL_INFO <= ITTMAXLOGLEVEL
 #define ITTDINFO(xx, ...) ITTDPRINT(xx, ##__VA_ARGS__)
 #else
 #define ITTDINFO(xx, ...) ((void)0)
 #endif
+
 #ifdef ITTDEBUG
 #define ITTDCONDITIONLOG(condition, xx, ...) { if ((condition)) { \
 ITTDPRINT(xx, ##__VA_ARGS__); \
@@ -79,6 +92,7 @@ ITTDPRINT(xx, ##__VA_ARGS__); \
 #else
 #define ITTDCONDITIONLOG(condition, xx, ...) ((void)0)
 #endif
+
 #define ITTAssert(condition, ...) \
 do { \
 if (!(condition)) { \
@@ -90,16 +104,22 @@ description:__VA_ARGS__]; \
 } \
 } while(0)
 //---------------------打印日志--------------------------
-//----------------------系统----------------------------
+
+
+
+#pragma mark ----------------------系统----------------------------
 //获取系统版本
-#define IOS_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
-#define CurrentSystemVersion [[UIDevice currentDevice] systemVersion]
+#define kCurrentSystemVersionFloat [[[UIDevice currentDevice] systemVersion] floatValue]
+#define kCurrentSystemVersionString [[UIDevice currentDevice] systemVersion]
+
 //获取当前语言
-#define CurrentLanguage ([[NSLocale preferredLanguages] objectAtIndex:0])
+#define kCurrentLanguage ([[NSLocale preferredLanguages] objectAtIndex:0])
+
 //判断是否 Retina屏、设备是否%fhone 5、是否是iPad
 #define isRetina ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) : NO)
 #define iPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
 #define isPad (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+
 //判断是真机还是模拟器
 #if TARGET_OS_IPHONE
 //iPhone Device
@@ -107,6 +127,8 @@ description:__VA_ARGS__]; \
 #if TARGET_IPHONE_SIMULATOR
 //iPhone Simulator
 #endif
+
+
 //检查系统版本
 #define SYSTEM_VERSION_EQUAL_TO(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
 #define SYSTEM_VERSION_GREATER_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
@@ -114,7 +136,9 @@ description:__VA_ARGS__]; \
 #define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
 #define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
 //----------------------系统----------------------------
-//----------------------内存----------------------------
+
+
+#pragma mark ----------------------内存----------------------------
 //使用ARC和不使用ARC
 #if __has_feature(objc_arc)
 //compiling with ARC
@@ -127,7 +151,10 @@ description:__VA_ARGS__]; \
 #define SAFE_DELETE(P) if(P) { [P release], P = nil; }
 #define SAFE_RELEASE(x) [x release];x=nil
 //----------------------内存----------------------------
-//----------------------图片----------------------------
+
+
+
+#pragma mark ----------------------图片----------------------------
 //读取本地图片
 #define LOADIMAGE(file,ext) [UIImage imageWithContentsOfFile:[[NSBundle mainBundle]pathForResource:file ofType:ext]]
 //定义UIImage对象
@@ -136,7 +163,10 @@ description:__VA_ARGS__]; \
 #define ImageNamed(_pointer) [UIImage imageNamed:[UIUtil imageName:_pointer]]
 //建议使用前两种宏定义,性能高于后者
 //----------------------图片----------------------------
-//----------------------颜色类---------------------------
+
+
+
+#pragma mark ----------------------颜色类---------------------------
 // rgb颜色转换（16进制->10进制）
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 //带有RGBA的颜色设置
@@ -152,7 +182,21 @@ description:__VA_ARGS__]; \
 #define RGBCOLOR(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]
 #define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
 //----------------------颜色类--------------------------
-//----------------------其他----------------------------
+
+
+
+
+
+#pragma mark ———————————字符串----------------------------
+/* 不同系统计算字符串高度 不用加 ;  iOS7的比iOS6宽 */
+#define KcalculateStringSize(strSize,string,fontSize,sizeWidth,sizeHeight)  CGSize strSize;  if(kCurrentSystemVersionFloat>=7.0)\
+{\
+NSDictionary *attribute = @{NSFontAttributeName: [UIFont systemFontOfSize:fontSize]};\
+strSize = [string boundingRectWithSize:CGSizeMake(sizeWidth, sizeHeight) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:attribute context:nil].size;\
+}
+
+
+#pragma mark ----------------------其他----------------------------
 //方正黑体简体字体定义
 #define FONT(F) [UIFont fontWithName:@"FZHTJW--GB1-0" size:F]
 //定义一个API
@@ -206,13 +250,6 @@ return nil; \
 - (id)copyWithZone:(NSZone *)zone \
 { \
 return self; \
-}
-//———————————字符串----------------------------
-/* 不同系统计算字符串高度 不用加 ;  iOS7的比iOS6宽 */
-#define KcalculateStringSize(name,string,fontSize,sizeWidth,sizeHeight)  CGSize name;  if([[[UIDevice currentDevice]systemVersion]floatValue]>=7.0)\
-{\
-NSDictionary *attribute = @{NSFontAttributeName: [UIFont systemFontOfSize:fontSize]};\
-name = [string boundingRectWithSize:CGSizeMake(sizeWidth, sizeHeight) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:attribute context:nil].size;\
 }
 
 
