0b86aa611aadd2cfd0ceb3451bed792e79159420
pwn1os <qazx84265@163.com>
2017-03-29 14:23:34 +0800

    Update FBProgressWebViewController.m

--- CommonTools/FBProgressWebViewController.m
+++ CommonTools/FBProgressWebViewController.m
@@ -282,10 +282,6 @@
 }
 
 
-
-
-
-
 /**
  *  kvo
  *
@@ -319,12 +315,6 @@
         decisionHandler(WKNavigationActionPolicyAllow);
     }
 }
-
-
-
-
-
-
 
 - (void)setLoadCount:(NSUInteger)loadCount {
     
@@ -376,19 +366,153 @@
     }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+//- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+//    
+//    NSURL *url = [request URL];
+//    NSString *urlStr = url.absoluteString;
+//    
+//    return [self processURL:urlStr];
+//    
+//}
+//
+//- (BOOL) processURL:(NSString *) url
+//{
+//    NSString *urlStr = [NSString stringWithString:url];
+//    
+//    NSString *protocolPrefix = @"js2ios://";
+//    
+//    //process only our custom protocol
+//    if ([[urlStr lowercaseString] hasPrefix:protocolPrefix])
+//    {
+//        //strip protocol from the URL. We will get input to call a native method
+//        urlStr = [urlStr substringFromIndex:protocolPrefix.length];
+//        
+//        //Decode the url string
+//        urlStr = [urlStr stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+//        
+//        NSError *jsonError;
+//        
+//        //parse JSON input in the URL
+//        NSDictionary *callInfo = [NSJSONSerialization
+//                                  JSONObjectWithData:[urlStr dataUsingEncoding:NSUTF8StringEncoding]
+//                                  options:kNilOptions
+//                                  error:&jsonError];
+//        
+//        //check if there was error in parsing JSON input
+//        if (jsonError != nil)
+//        {
+//            NSLog(@"Error parsing JSON for the url %@",url);
+//            return NO;
+//        }
+//        
+//        //Get function name. It is a required input
+//        NSString *functionName = [callInfo objectForKey:@"functionname"];
+//        if (functionName == nil)
+//        {
+//            NSLog(@"Missing function name");
+//            return NO;
+//        }
+//        
+//        NSString *successCallback = [callInfo objectForKey:@"success"];
+//        NSString *errorCallback = [callInfo objectForKey:@"error"];
+//        NSArray *argsArray = [callInfo objectForKey:@"args"];
+//        
+//        [self callNativeFunction:functionName withArgs:argsArray onSuccess:successCallback onError:errorCallback];
+//        
+//        //Do not load this url in the WebView
+//        return NO;
+//        
+//    }
+//    
+//    return YES;
+//}
+//
+//- (void) callNativeFunction:(NSString *) name withArgs:(NSArray *) args onSuccess:(NSString *) successCallback onError:(NSString *) errorCallback
+//{
+//    //We only know how to process sayHello
+//    if ([name compare:@"sayHello" options:NSCaseInsensitiveSearch] == NSOrderedSame)
+//    {
+//        if (args.count > 0)
+//        {
+//            NSString *resultStr = [NSString stringWithFormat:@"Hello %@ !", [args objectAtIndex:0]];
+//            
+//            [self callSuccessCallback:successCallback withRetValue:resultStr forFunction:name];
+//        }
+//        else
+//        {
+//            NSString *resultStr = [NSString stringWithFormat:@"Error calling function %@. Error : Missing argument", name];
+//            [self callErrorCallback:errorCallback withMessage:resultStr];
+//        }
+//    }
+//    else
+//    {
+//        //Unknown function called from JavaScript
+//        NSString *resultStr = [NSString stringWithFormat:@"Cannot process function %@. Function not found", name];
+//        [self callErrorCallback:errorCallback withMessage:resultStr];
+//        
+//    }
+//}
+//
+//-(void) callErrorCallback:(NSString *) name withMessage:(NSString *) msg
+//{
+//    if (name != nil)
+//    {
+//        //call error handler
+//        
+//        NSMutableDictionary *resultDict = [[NSMutableDictionary alloc] init];
+//        [resultDict setObject:msg forKey:@"error"];
+//        [self callJSFunction:name withArgs:resultDict];
+//    }
+//    else
+//    {
+//        NSLog(@"%@",msg);
+//    }
+//    
+//}
+//
+//-(void) callSuccessCallback:(NSString *) name withRetValue:(id) retValue forFunction:(NSString *) funcName
+//{
+//    if (name != nil)
+//    {
+//        //call succes handler
+//        
+//        NSMutableDictionary *resultDict = [[NSMutableDictionary alloc] init];
+//        [resultDict setObject:retValue forKey:@"result"];
+//        [self callJSFunction:name withArgs:resultDict];
+//    }
+//    else
+//    {
+//        NSLog(@"Result of function %@ = %@", funcName,retValue);
+//    }
+//    
+//}
+//
+//-(void) callJSFunction:(NSString *) name withArgs:(NSMutableDictionary *) args
+//{
+//    NSError *jsonError;
+//    
+//    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:args options:0 error:&jsonError];
+//    
+//    if (jsonError != nil)
+//    {
+//        //call error callback function here
+//        NSLog(@"Error creating JSON from the response  : %@",[jsonError localizedDescription]);
+//        return;
+//    }
+//    
+//    //initWithBytes:length:encoding
+//    NSString *jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+//    
+//    NSLog(@"jsonStr = %@", jsonStr);
+//    
+//    if (jsonStr == nil)
+//    {
+//        NSLog(@"jsonStr is null. count = %zd", [args count]);
+//    }
+//    
+//    [self.wkwebView evaluateJavaScript:[NSString stringWithFormat:@"%@('%@');",name,jsonStr] completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+//        
+//    }];
+//}
 
 @end
